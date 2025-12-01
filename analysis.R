04cf08bad603f14e2fa4da0e56b084ee1e838a00
m2mosig-ridenee <eya.ridene@grenoble-inp.org>
2025-12-01 19:24:12 +0100

    Add the R Analysis & ggplot2 Figures

---
+++ analysis.R
@@ -0,0 +1,46 @@
+library(ggplot2)
+library(dplyr)
+library(scales)
+library(tidyr)
+
+
+# Load data
+df <- read.csv("./data/LAPTOP-SMTR1LU7_2025-12-01/measurments_17:46.csv") 
+
+# ===========================================
+# 1. Summary statistics + confidence intervals
+# ===========================================
+
+summary_df <- df %>%
+  group_by(Size, Type) %>%
+  summarise(
+    mean_time = mean(Time),
+    sd_time = sd(Time),
+    n = n(),
+    se = sd_time / sqrt(n),
+    ci95 = 1.96 * se,
+    .groups = "drop"
+  )
+
+print(summary_df)
+
+# ===========================================
+# 2. Mean plot + confidence intervals
+# ===========================================
+
+p1 <- ggplot(summary_df, aes(x = Size, y = mean_time, color = Type)) +
+  geom_line(linewidth=1) +
+  geom_point(size=3) +
+  geom_errorbar(aes(ymin = mean_time - ci95, ymax = mean_time + ci95),
+                width = 0.1, alpha = 0.6) +
+  geom_smooth(se = TRUE, method = "loess", linewidth = 1.2, alpha = 0.2) +
+  scale_x_log10(labels = comma) +
+  scale_y_log10(labels = comma) +
+  theme_bw(base_size = 14) +
+  labs(
+    title = "Execution Time of Quicksort Variants (with 95% CI)",
+    x = "Array Size (log scale)",
+    y = "Execution Time (seconds, log scale)"
+  )
+
+ggsave("performance_plot.png", p1, width = 8, height = 6)
