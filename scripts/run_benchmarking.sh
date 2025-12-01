e914dd25905d9ba2aeea08a492eda8f08dd5b0e1
m2mosig-ridenee <eya.ridene@grenoble-inp.org>
2025-12-01 19:24:25 +0100

    Improve the Experimental Design

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -4,8 +4,13 @@ mkdir -p $OUTPUT_DIRECTORY
 OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_`date +%R`.txt
 
 touch $OUTPUT_FILE
-for i in 100 1000 10000 100000 1000000; do
-    for rep in `seq 1 5`; do
+# Add more array sizes
+sizes="100 500 1000 5000 10000 50000 100000 200000 500000 1000000 2000000 5000000"
+# Randomize the order of the runs
+shuf_sizes=$(echo $sizes | tr ' ' '\n' | shuf)
+for i in $shuf_sizes; do
+# Increase the number of repetitions
+    for rep in `seq 1 30`; do
         echo "Size: $i" >> $OUTPUT_FILE;
         ./src/parallelQuicksort $i >> $OUTPUT_FILE;
     done ;
